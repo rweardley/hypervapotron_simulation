d73b0ae3d0171f9754fd58eda4ae418d7b61af4c
Rupert Eardley-Brunt <rupert.eardley-brunt@ukaea.uk>
2024-11-27 21:47:31 +0000

    Set case up to run on Bede with GPU-aware MPI on 5 nodes

--- hvt_fluid_gh/run.sh
+++ hvt_fluid_gh/run.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-source $HOME/rupert/.nekrs_next_241115_profile
-nrsmpi hvt 8 2>&1 | tee log.run
+source ~/.nekrs_next_241127_1121_profile
 
+export NEKRS_GPU_MPI=1
+export UCX_RNDV_THRESH=1024
+export UCX_RC_TM_ENABLE=y
+
+# MPI is set up to use slurm to check allocation
+# so this will run 1 rank per node allocated
+mpirun -N 1 nekrs --setup hvt 1 2>&1 | tee log.run
